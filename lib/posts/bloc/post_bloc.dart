4e8d7cc98d97e35aa8851a8642bb53f38814fa62
zdravkostanin1 <zdravko.stanin@gmail.com>
2023-10-16 11:56:10 +0300

    adding http package and implementing bloc: event, state and bloc

--- lib/posts/bloc/post_bloc.dart
+++ lib/posts/bloc/post_bloc.dart
@@ -1,13 +1,15 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+import '../models/post.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
-  PostBloc() : super(PostInitial()) {
-    on<PostEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  PostBloc({required this.httpClient}) : super(const PostState()) {
+    /// TODO: register on<PostFetched> event
   }
+
+  final http.Client httpClient;
 }
--- lib/posts/bloc/post_event.dart
+++ lib/posts/bloc/post_event.dart
@@ -2,4 +2,10 @@ part of 'post_bloc.dart';
 
 abstract class PostEvent extends Equatable {
   const PostEvent();
+
+  @override
+  List<Object> get props => [];
 }
+
+/// a single [event] for fetching [posts]
+final class PostFetched extends PostEvent {}
