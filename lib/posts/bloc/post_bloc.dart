782485ee1c5b56c703cb002a8c9d68b7998e0703
zdravkostanin1 <zdravko.stanin@gmail.com>
2023-10-16 11:44:21 +0300

    fixing imports

--- lib/posts/bloc/post_bloc.dart
+++ lib/posts/bloc/post_bloc.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
-
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
